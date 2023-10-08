7cb5e0ba38c2bdb1453110af5869fe02c00c1c16
TPDs <mig.mic@gmail.com>
2023-10-09 01:36:00 +0200

    last of the day

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from pathlib import Path
 from dotenv import load_dotenv
+import uvicorn
 from util.pocketbase import pb_db
 import json
 
@@ -13,7 +15,8 @@ pb = pb_db().get_db()
 async def root():
     with open('testdata.json') as f:
         data = json.load(f)
-    result =  pb.collection('project').get_full_list()
-    print(data)
-    
-    return result
+    result = pb.collection('project').get_full_list() 
+    return data
+
+if __name__ == '__main__':
+    uvicorn.run(f"{Path(__file__).stem}:app", port=5273, host='127.0.0.1', reload=True)
--- util/pocketbase.py
+++ util/pocketbase.py
@@ -1,5 +1,4 @@
-from pocketbase import PocketBase  # Client also works the same
-from pocketbase.client import FileUpload
+from pocketbase import PocketBase 
 import os
 from dotenv import load_dotenv
 
@@ -9,19 +8,19 @@ PUBLIC_PB_URL = os.getenv('PUBLIC_PB_URL')
 PUBLIC_PB_ADMIN = os.getenv('PUBLIC_PB_ADMIN')
 PUBLIC_PB_PW = os.getenv('PUBLIC_PB_PW')
 
-client = PocketBase(PUBLIC_PB_URL)
+client = PocketBase(PUBLIC_PB_URL) # type: ignore
 
 class pb_db:
     __instance__ = None
-
+    
     def __int__(self):
         if pb_db.__instance__ is None:
-            pb_db.__instance__ = client.admins.auth_with_password(PUBLIC_PB_ADMIN, PUBLIC_PB_PW)
+            pb_db.__instance__ = client.admins.auth_with_password(PUBLIC_PB_ADMIN, PUBLIC_PB_PW) # type: ignore
         else:
             raise Exception("DB Conn findes allerede")
 
     @staticmethod
     def get_db():
-        if not pb_db.__instance__:
-            pb_db.__instance__ = client.admins.auth_with_password(PUBLIC_PB_ADMIN, PUBLIC_PB_PW)
+        if not pb_db.__instance__:            
+            pb_db.__instance__ = client.admins.auth_with_password(PUBLIC_PB_ADMIN, PUBLIC_PB_PW) # type: ignore
         return client
