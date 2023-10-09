075e767b518bfa1034da706f52c55fa803241d7b
TPDs <mig.mic@gmail.com>
2023-10-09 12:54:21 +0200

    test

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 import uvicorn
 from util.pocketbase import pb_db
 import json
+import os
 
 load_dotenv() 
-
+host = os.getenv('HOST')
 app = FastAPI()
 pb = pb_db().get_db()
 
@@ -19,4 +20,4 @@ async def root():
     return data
 
 if __name__ == '__main__':
-    uvicorn.run(f"{Path(__file__).stem}:app", port=5273, host='127.0.0.1', reload=True)
+    uvicorn.run(f"{Path(__file__).stem}:app", port=5273, host=host, reload=True) # type: ignore
